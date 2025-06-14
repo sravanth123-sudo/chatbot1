734c659c6a0f76efe4fddda5b5610a895109f5ed
sravanth123-sudo <sravanthreddyyannam@gmail.com>
2025-06-14 20:15:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,50 @@
 import streamlit as st
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+#from config import CHAT_PROMPT_TEMPLATE
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# Title on the page
+st.markdown(
+    "<h2 style='text-align: center; color: #4CAF50; font-family: Arial;'>Hermione🪶</h2>",
+    unsafe_allow_html=True,
 )
+
+CHAT_PROMPT_TEMPLATE = """
+You are Hermione Granger from Harry Potter. 
+You know everything and aim to provide a concise response.
+User: {question}
+Assistant: """
+
+template = CHAT_PROMPT_TEMPLATE
+prompt = ChatPromptTemplate.from_template(template)
+
+#Load the local Llama3.2 model that we pulled using ollama
+model = OllamaLLM(model="llama3.2")
+chain = prompt | model
+
+#Initialize message history in session state
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Hi! How may I help you?"}
+    ]
+
+#Display the chat history
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+# Handle user input
+if user_input := st.chat_input("What is up?"):
+    # Add user message to session state
+    st.session_state.messages.append({"role": "user", "content": user_input})
+    with st.chat_message("user"):
+        st.markdown(user_input)
+
+    # Generate assistant response
+    with st.chat_message("assistant"):
+        response = chain.invoke({"question": user_input})
+        st.markdown(response)
+
+    # Add assistant response to session state
+    st.session_state.messages.append({"role": "assistant", "content": response})
